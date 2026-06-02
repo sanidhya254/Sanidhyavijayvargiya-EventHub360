6e038e0ae56997bef39c3adcf29c33c6a0ead8de
sanidhya254 <sanidhyavijayvargiya4@gmail.com>
2026-06-02 18:37:55 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const authRoutes = require("./routes/auth");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+
+app.listen(process.env.PORT, () => {
+  console.log(
+    `Server running on port ${process.env.PORT}`
+  );
+});
